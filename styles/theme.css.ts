8092b38c25fb2d17ab23eacadf5921536ddc671d
양희준 <88187322+YHJ96@users.noreply.github.com>
2023-12-28 11:37:15 +0900

    refactor: createTheme cssVar 변수 추출

--- styles/theme.css.ts
+++ styles/theme.css.ts
@@ -1,20 +1,24 @@
 import { createTheme } from '@vanilla-extract/css';
 
-// 테마 만들어 사용하기 var 모듈화
-const theme = createTheme({
+// 기본 CSS 변수를 따로 관리
+const defaultVars = {
   color: {
     primary: 'blue',
     danger: 'red',
   },
-});
+};
+
+// 테마 만들어 사용하기 var 모듈화
+const theme = createTheme(defaultVars);
 
 const [_, vars] = theme;
 
 // 모든 테마 제공해야함 새로운 ClassName 반환 해당 타입 return string 타입 확인 완료
+// 스프레드를 사용하여 유용하게 대처할 수 있도록 하면 좋은거 같음
 const variantTheme = createTheme(vars, {
   color: {
+    ...defaultVars.color,
     primary: 'skyblue',
-    danger: 'red',
   },
 });
 
